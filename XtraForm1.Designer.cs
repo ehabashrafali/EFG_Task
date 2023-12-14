b69b50b47ebf017ff83db1439e6104f17bc4894e
ehab <ehabashrafali88@gmail.com>
2023-12-14 02:41:14 +0200

    first commit

---
+++ XtraForm1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace DataControl_Task
+{
+    partial class XtraForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.employeeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colFname = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.repositoryItemGridLookUpEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemGridLookUpEdit();
+            this.departmentBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.repositoryItemGridLookUpEdit1View = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colLname = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colSSN = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colBdate = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colAddress = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colSex = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colSalary = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colSuperssn = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colDno = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.tabFormControl1 = new DevExpress.XtraBars.TabFormControl();
+            this.tabFormPage1 = new DevExpress.XtraBars.TabFormPage();
+            this.tabFormContentContainer1 = new DevExpress.XtraBars.TabFormContentContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemGridLookUpEdit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.departmentBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemGridLookUpEdit1View)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabFormControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataSource = this.employeeBindingSource;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.EmbeddedNavigator.Appearance.ForeColor = System.Drawing.Color.Black;
+            this.gridControl1.EmbeddedNavigator.Appearance.Options.UseForeColor = true;
+            this.gridControl1.Location = new System.Drawing.Point(0, 71);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repositoryItemGridLookUpEdit1});
+            this.gridControl1.Size = new System.Drawing.Size(862, 377);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            this.gridControl1.Click += new System.EventHandler(this.gridControl1_Click);
+            // 
+            // employeeBindingSource
+            // 
+            this.employeeBindingSource.DataSource = typeof(DataControl_Task.Employee);
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colFname,
+            this.colLname,
+            this.colSSN,
+            this.colBdate,
+            this.colAddress,
+            this.colSex,
+            this.colSalary,
+            this.colSuperssn,
+            this.colDno});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // colFname
+            // 
+            this.colFname.ColumnEdit = this.repositoryItemGridLookUpEdit1;
+            this.colFname.FieldName = "Fname";
+            this.colFname.Name = "colFname";
+            this.colFname.Visible = true;
+            this.colFname.VisibleIndex = 0;
+            // 
+            // repositoryItemGridLookUpEdit1
+            // 
+            this.repositoryItemGridLookUpEdit1.AdvancedModeOptions.SelectionColor = System.Drawing.Color.Black;
+            this.repositoryItemGridLookUpEdit1.AutoHeight = false;
+            this.repositoryItemGridLookUpEdit1.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.repositoryItemGridLookUpEdit1.DataSource = this.departmentBindingSource;
+            this.repositoryItemGridLookUpEdit1.DisplayMember = "Dname";
+            this.repositoryItemGridLookUpEdit1.Name = "repositoryItemGridLookUpEdit1";
+            this.repositoryItemGridLookUpEdit1.PopupView = this.repositoryItemGridLookUpEdit1View;
+            this.repositoryItemGridLookUpEdit1.ValueMember = "Dnum";
+            // 
+            // departmentBindingSource
+            // 
+            this.departmentBindingSource.DataSource = typeof(DataControl_Task.Department);
+            // 
+            // repositoryItemGridLookUpEdit1View
+            // 
+            this.repositoryItemGridLookUpEdit1View.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus;
+            this.repositoryItemGridLookUpEdit1View.Name = "repositoryItemGridLookUpEdit1View";
+            this.repositoryItemGridLookUpEdit1View.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.repositoryItemGridLookUpEdit1View.OptionsView.ShowGroupPanel = false;
+            // 
+            // colLname
+            // 
+            this.colLname.FieldName = "Lname";
+            this.colLname.Name = "colLname";
+            this.colLname.OptionsColumn.AllowEdit = false;
+            this.colLname.Visible = true;
+            this.colLname.VisibleIndex = 1;
+            // 
+            // colSSN
+            // 
+            this.colSSN.FieldName = "SSN";
+            this.colSSN.Name = "colSSN";
+            this.colSSN.Visible = true;
+            this.colSSN.VisibleIndex = 2;
+            // 
+            // colBdate
+            // 
+            this.colBdate.FieldName = "Bdate";
+            this.colBdate.Name = "colBdate";
+            this.colBdate.Visible = true;
+            this.colBdate.VisibleIndex = 3;
+            // 
+            // colAddress
+            // 
+            this.colAddress.FieldName = "Address";
+            this.colAddress.Name = "colAddress";
+            this.colAddress.Visible = true;
+            this.colAddress.VisibleIndex = 4;
+            // 
+            // colSex
+            // 
+            this.colSex.FieldName = "Sex";
+            this.colSex.Name = "colSex";
+            this.colSex.Visible = true;
+            this.colSex.VisibleIndex = 5;
+            // 
+            // colSalary
+            // 
+            this.colSalary.FieldName = "Salary";
+            this.colSalary.Name = "colSalary";
+            this.colSalary.Visible = true;
+            this.colSalary.VisibleIndex = 6;
+            // 
+            // colSuperssn
+            // 
+            this.colSuperssn.FieldName = "Superssn";
+            this.colSuperssn.Name = "colSuperssn";
+            this.colSuperssn.Visible = true;
+            this.colSuperssn.VisibleIndex = 7;
+            // 
+            // colDno
+            // 
+            this.colDno.FieldName = "Dno";
+            this.colDno.Name = "colDno";
+            this.colDno.Visible = true;
+            this.colDno.VisibleIndex = 8;
+            // 
+            // tabFormControl1
+            // 
+            this.tabFormControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabFormControl1.Name = "tabFormControl1";
+            this.tabFormControl1.Pages.Add(this.tabFormPage1);
+            this.tabFormControl1.SelectedPage = this.tabFormPage1;
+            this.tabFormControl1.Size = new System.Drawing.Size(862, 71);
+            this.tabFormControl1.TabForm = this;
+            this.tabFormControl1.TabIndex = 1;
+            this.tabFormControl1.TabStop = false;
+            this.tabFormControl1.Click += new System.EventHandler(this.tabFormControl1_Click);
+            // 
+            // tabFormPage1
+            // 
+            this.tabFormPage1.ContentContainer = this.tabFormContentContainer1;
+            this.tabFormPage1.Name = "tabFormPage1";
+            this.tabFormPage1.Text = "Page 0";
+            // 
+            // tabFormContentContainer1
+            // 
+            this.tabFormContentContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabFormContentContainer1.Location = new System.Drawing.Point(0, 71);
+            this.tabFormContentContainer1.Name = "tabFormContentContainer1";
+            this.tabFormContentContainer1.Size = new System.Drawing.Size(862, 377);
+            this.tabFormContentContainer1.TabIndex = 2;
+            // 
+            // XtraForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 448);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.tabFormContentContainer1);
+            this.Controls.Add(this.tabFormControl1);
+            this.Name = "XtraForm1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TabFormControl = this.tabFormControl1;
+            this.Text = "XtraForm1";
+            this.Load += new System.EventHandler(this.XtraForm1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemGridLookUpEdit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.departmentBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemGridLookUpEdit1View)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabFormControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private System.Windows.Forms.BindingSource employeeBindingSource;
+        private DevExpress.XtraGrid.Columns.GridColumn colFname;
+        private DevExpress.XtraGrid.Columns.GridColumn colLname;
+        private DevExpress.XtraGrid.Columns.GridColumn colSSN;
+        private DevExpress.XtraGrid.Columns.GridColumn colBdate;
+        private DevExpress.XtraGrid.Columns.GridColumn colAddress;
+        private DevExpress.XtraGrid.Columns.GridColumn colSex;
+        private DevExpress.XtraGrid.Columns.GridColumn colSalary;
+        private DevExpress.XtraGrid.Columns.GridColumn colSuperssn;
+        private DevExpress.XtraGrid.Columns.GridColumn colDno;
+        private DevExpress.XtraBars.TabFormControl tabFormControl1;
+        private DevExpress.XtraBars.TabFormPage tabFormPage1;
+        private DevExpress.XtraBars.TabFormContentContainer tabFormContentContainer1;
+        private DevExpress.XtraEditors.Repository.RepositoryItemGridLookUpEdit repositoryItemGridLookUpEdit1;
+        private System.Windows.Forms.BindingSource departmentBindingSource;
+        private DevExpress.XtraGrid.Views.Grid.GridView repositoryItemGridLookUpEdit1View;
+    }
+}
---
+++ XtraForm1.cs
@@ -0,0 +1,38 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataControl_Task
+{
+    public partial class XtraForm1 : DevExpress.XtraBars.TabForm
+    {
+        public XtraForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void XtraForm1_Load(object sender, EventArgs e)
+        {
+            Company_SDEntities company_SDEntities = new Company_SDEntities();
+            employeeBindingSource.DataSource = company_SDEntities.Employees.ToList();
+            departmentBindingSource.DataSource = company_SDEntities.Departments.ToList();
+        }
+
+        private void gridControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabFormControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
